38e548e9697315fc706e9885c34f2d4b25cea5e8
Nwosa <obere4u@gmail.com>
2023-03-30 02:57:38 +0100

    Added Responsiveness to the app, changed the name

--- main.js
+++ main.js
@@ -2,64 +2,70 @@
 const body = document.querySelector("body");
 const todoContainer = document.querySelector(".todo-container");
 const inputTodo = document.querySelector("#todo-input");
-const buttonAddTodo = document.querySelector(".todo-button");
+const buttonAddTodo = document.querySelector(".add-button");
 const listTodo = document.querySelector(".todo-list");
 const warningFilter = document.querySelector("#filter-warning")
 const warningInputTodo = document.querySelector("#input-warning");
 const optionFilterTodos = document.querySelector(".filter-todos");
 const toggleThemeButton = document.querySelector("#toggle-theme-button");
+const voiceCommandButton = document.querySelector("#voice-command-button");
+
 
 //Event Listeners
 document.addEventListener("DOMContentLoaded", getTodosFromLocalStorage); //saves the content when page refreshed/reload
 toggleThemeButton.addEventListener("click", darkOrLight);
-buttonAddTodo.addEventListener("click", addToTodoList);
+buttonAddTodo.addEventListener("click", addTodoList); //checks if the addTodo button was clicked
+inputTodo.addEventListener("keydown", addTodoList); //checks if the event was triggered by either the button click or the Enter key press and proceeds with adding the todo accordingly
 listTodo.addEventListener("click", deleteOrCheckTodo);
 optionFilterTodos.addEventListener("click", filterTodos);
+voiceCommandButton.addEventListener("click", startSpeechRecognition);
 
 //Functions
 //Add to List
-function addToTodoList(e) {
-  e.preventDefault(); //stops the event from acting in it's default setting
+function addTodoList(e) {
+  // Check if the event was triggered by the "Add Todo" button or the Enter key
+  if (e.type === "click" || (e.type === "keydown" && e.key === "Enter")) {
+    e.preventDefault(); //stops the event from acting in it's default setting
 
-  const todoText = inputTodo.value.trim();
-  if (todoText === "") {
-    warningInputTodo.style.display = "block";
-    return;
-  } else {
-    warningInputTodo.style.display = "none";
+    const todoText = inputTodo.value.trim();
+    if (todoText === "") {
+      warningInputTodo.style.display = "block";
+      return;
+    } else {
+      warningInputTodo.style.display = "none";
+    }
+
+    //Todo div
+    const todoDiv = document.createElement("div");
+    todoDiv.classList.add("todo");
+    
+
+    //Create a List (li) item
+    const newTodo = document.createElement("li");
+    newTodo.innerText = inputTodo.value;
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+
+    //Check completed button
+    const completeButton = document.createElement("button");
+    completeButton.innerHTML =
+      '<ion-icon name="checkmark-circle-sharp"></ion-icon>';
+    completeButton.classList.add("complete-btn");
+    todoDiv.appendChild(completeButton);
+
+    //check trash button
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML = '<ion-icon name="trash-sharp"></ion-icon>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+
+    //Append to List
+    listTodo.appendChild(todoDiv);
+
+    inputTodo.value = ""; //clears the inputTodo space
+    //Add to local Storage
+    saveTodosLocally(newTodo.innerText);
   }
-
-  //Todo div
-  const todoDiv = document.createElement("div");
-  todoDiv.classList.add("todo");
-  
-
-  //Create a List (li) item
-  const newTodo = document.createElement("li");
-  newTodo.innerText = inputTodo.value;
-  newTodo.classList.add("todo-item");
-  todoDiv.appendChild(newTodo);
-
-  //Check completed button
-  const completeButton = document.createElement("button");
-  completeButton.innerHTML =
-    '<ion-icon name="checkmark-circle-sharp"></ion-icon>';
-  completeButton.classList.add("complete-btn");
-  todoDiv.appendChild(completeButton);
-
-  //check trash button
-  const trashButton = document.createElement("button");
-  trashButton.innerHTML = '<ion-icon name="trash-sharp"></ion-icon>';
-  trashButton.classList.add("trash-btn");
-  todoDiv.appendChild(trashButton);
-
-  //Append to List
-  listTodo.appendChild(todoDiv);
-
-  inputTodo.value = " "; //clears the inputTodo space
-  
-  //Add to local Storage
-  saveTodosLocally(inputTodo.value);
 }
 
 function deleteOrCheckTodo(e) {
@@ -76,6 +82,7 @@ function deleteOrCheckTodo(e) {
     const todo = item.parentElement;
     todo.classList.toggle("complete");
   }
+  
 }
 
 // Set the default theme to the previously selected theme (if any), or to light mode
@@ -94,8 +101,6 @@ if (theme === "dark-mode") {
   warningInputTodo.classList.remove("dark-mode");
 }
 
-console.log("Current theme:", theme);
-
 // Toggle between Dark and Light Mode
 function darkOrLight(e) {
   if (e.target.classList.contains("dark-toggle")) {
@@ -108,7 +113,6 @@ function darkOrLight(e) {
     body.classList.remove("light-mode");
     body.classList.add("dark-mode");
     todoContainer.classList.add("dark-mode");
-    console.log("dark mode applied to todo-container");
     toggleThemeButton.classList.add("dark-mode");
     localStorage.setItem("theme", "dark-mode"); // save the current theme mode in local storage
   } else {
@@ -121,15 +125,12 @@ function darkOrLight(e) {
     body.classList.remove("dark-mode");
     body.classList.add("light-mode");
     todoContainer.classList.remove("dark-mode");
-    console.log("light mode applied to todo-container");
-    toggleThemeButton.classList.remove("dark-mode");
     localStorage.setItem("theme", "light-mode"); // save the current theme mode in local storage
   }
   
   const newTodo = document.createElement("li");
   newTodo.innerText = inputTodo.value;
   newTodo.classList.add("list-item");
-  console.log("Current theme", theme);
 }
 
 function filterTodos() {
@@ -177,13 +178,12 @@ function filterTodos() {
 
 function saveTodosLocally(todo) {
   //Check if the data exist in the local storage
-
   let todos;
 
   if (localStorage.getItem("todos") === null) {
-      todos = [];
+    todos = [];
   } else {
-      todos = JSON.parse(localStorage.getItem("todos"));
+    todos = JSON.parse(localStorage.getItem("todos"));
   }
 
   todos.push(todo);
@@ -228,8 +228,40 @@ function getTodosFromLocalStorage() {
     //Append to List
     listTodo.appendChild(todoDiv);
 
-    //Add to local Storage
-    saveTodosLocally(todo);
-    localStorage.clear(todo);
   });
+  //Add to local Storage
+  saveTodosLocally(todos);
 };
+
+// Function to start speech recognition
+function startSpeechRecognition() {
+  const recognition = new window.webkitSpeechRecognition();
+  recognition.lang = "en-US";
+
+  recognition.onresult = deleteOrCheckTodo;
+  recognition.start();
+}
+
+function startSpeechRecognition(e) {
+  e.preventDefault();
+  const recognition = new window.webkitSpeechRecognition();
+  recognition.continuous = false;
+  recognition.lang = "en-US";
+  recognition.interimResults = false;
+  recognition.maxAlternatives = 1;
+  recognition.onresult = deleteOrCheckTodo;
+  recognition.start();
+
+  recognition.onresult = function (e) {
+    const transcript = e.results[0][0].transcript.trim();
+    inputTodo.value = transcript;
+    deleteOrCheckTodo(e);
+    addToTodoList(e);
+    console.log("first");
+  };
+
+  recognition.onerror = function (e) {
+    console.error("Speech recognition error:", e.error);
+  };
+}
+
